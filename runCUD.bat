ff6ce754f8fcc21c1e54154ccc8e495ad5f91aac
Noah Dolev <noah.dolev@weizmann.ac.il>
2019-11-24 11:17:32 +0200

    added a comment to runCUD.bat

--- runCUD.bat
+++ runCUD.bat
@@ -1,3 +1,4 @@
 ECHO Set your python path as cudpy in system variables. 
+ECHO Serena
 ECHO Set the path to the runCUD.py in the NET repo as NETpath in system variables.  
 "C:\Users\bndolev.WISMAIN\AppData\Local\Continuum\anaconda3\Scripts\activate.bat" %cudpy% && python %NETpath%
