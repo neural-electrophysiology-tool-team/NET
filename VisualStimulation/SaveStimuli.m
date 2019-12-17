bbf9faa619dd7063b0f27de184e1bbbdb99b7673
RetinaExperimentor <RetinaExperimentor@PC01235227.wismain.weizmann.ac.il>
2019-12-17 15:02:13 +0200

    fixed path issue with filesep in savestimuli function

--- VisualStimulation/SaveStimuli.m
+++ VisualStimulation/SaveStimuli.m
@@ -5,13 +5,16 @@ NSKToolBoxMainDir=fileparts(which('identifierOfMainDir4NSKToolBox'));
 NSKToolBoxMainDir=regexp(NSKToolBoxMainDir,filesep,'split');
 NSKToolBoxMainDir=fullfile(NSKToolBoxMainDir{1:end-1});
 % savepath=fscanf(fopen([NSKToolBoxMainDir filesep 'NET' filesep 'PCspecificFiles' filesep 'stimSavePath.txt']),'%c');
-savepath = strcat(obj.stimSavePath,filesep,obj.user,filesep);
 if IsWin
-    savepath= strrep(savepath,'\', '\\');
+    stimSavePath = strrep(obj.stimSavePath,'\', filesep);
+    savepath = strrep(char(strcat(stimSavePath,filesep,obj.user,filesep)), '\','\\');
 else
-    savepath= strrep(savepath,'\', '/');
+    stimSavePath= strrep(obj.stimSavePath,'\', filesep);
+    savepath = strrep(char(strcat(stimSavePath,filesep,obj.user,filesep)), '\',filesep);
 end
-filename = sprintf(strcat(savepath, stimname,'_%s.mat'), datestr(now,'mm_dd_yyyy_HHMM'));
+
+
+filename = sprintf(strcat([savepath,stimname,'_%s.mat']), datestr(now,'mm_dd_yyyy_HHMM'));
 save(filename, 'varargin', 'obj', '-v7.3');
 
 end
