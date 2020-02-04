ad79959df431ebf2946dc420199e79288f3b8625
MSI ComputeServer <corticodes@gmail.com>
2020-02-04 14:50:40 +0200

    Update MCH5Recording.m

--- timeSeriesViewer/dataRecordingObjects/@MCH5Recording/MCH5Recording.m
+++ timeSeriesViewer/dataRecordingObjects/@MCH5Recording/MCH5Recording.m
@@ -1,5 +1,5 @@
 classdef MCH5Recording < dataRecording
-    
+%     This is a comment
   %MCH5Recording Reads a h5 recording file exported by MCS DataManager.
   % This class is used to read h5 containing raw data and
   % trigger information (currently from digital in data) from a 
