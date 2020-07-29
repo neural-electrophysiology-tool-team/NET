6dcd9f9fbe13b7225bf25a6a387b00484998dc67
GuyHyman <60943471+GuyHyman@users.noreply.github.com>
2020-07-30 01:53:21 +0300

    Update frameTimeFromDiode.m
    added some documentation to frameTimeFromDiode

--- VisualStimulation/accessories/frameTimeFromDiode.m
+++ VisualStimulation/accessories/frameTimeFromDiode.m
@@ -11,7 +11,7 @@ function [frameShifts,upCross,downCross,T,transitionNotFound]=frameTimeFromDiode
 %                       T - digital data time stamps
 %                       transitionNotFound - if matching time stamp was not found and original trigger taken instead
 %
-% Last updated : 15/07/15
+% Last updated : 11.12.2019
 
 %% default variables
 tStart=0;
@@ -22,10 +22,10 @@ maxChunck=1000*60*20; %ms
 trialStartEndDigiTriggerNumbers=[3 4];
 analogChNum=[]; %this used to be 1, now Kwik's getAnalog finds on its own
 transition=[];
-delay2Shift=1.5/60*1000; %ms
-maxFrameDeviation=0.5/60*1000; %ms
+delay2Shift=1.5/60*1000; %this defines the interval window (in ms) between the digital trigger shoft and the frame shift on the screen
+maxFrameDeviation=0.5/60*1000; %the maximum possible variability of the screen flip times
 
-plotDiodeTransitions=0;
+plotDiodeTransitions=0;%to plot or not plot the transitions
 T=[]; %digital triggers in the recording
 
 %LPF parameters
@@ -58,8 +58,9 @@ for i=1:2:length(varargin)
 end
 
 %% Main function
-Fs=dataRecordingObj.samplingFrequency(1);
-frameSamples=round(1/60*Fs);
+%prepare the LPF
+Fs=dataRecordingObj.samplingFrequency(1);%sampling frequency of the continous channels (probe and analog)
+frameSamples=round(1/60*Fs);%the sampling frequecy for a 60Hz refresh rate
 F=F.designLowPass;
 
 %extract digital triggers times if they were not provided during input
@@ -68,7 +69,7 @@ if isempty(T)
     dataRecordingObj.includeOnlyDigitalDataInTriggers=1;
     T=dataRecordingObj.getTrigger; %extract digital triggers throughout the recording
 end
-
+% In case there are no trial triggers:
 if isempty(T{trialStartEndDigiTriggerNumbers(1)}) || isempty(T{trialStartEndDigiTriggerNumbers(2)})
     disp('No start trigger detected in recording!!! Aborting calculation!');
     frameShifts=[];upCross=[];downCross=[];
@@ -100,11 +101,11 @@ if ~noisyAnalog %if noisy, estimate for each chunk
     %estimate transition points
     if isempty(transition)
         hWB=waitbar(0,hWB,'Classifying transition on sample data...');
-        %take the analog data during the first 10 trials (if available) with length of double the trial size 
+        %take the analog data during 10 random trials (if available) with length of double the trial size 
         avgTrialDuration=round(mean(T{trialStartEndDigiTriggerNumbers(2)}-T{trialStartEndDigiTriggerNumbers(1)})*2);
         [Atmp]=dataRecordingObj.getAnalogData(analogChNum,T{trialStartEndDigiTriggerNumbers(1)}(randi([2, max(11,numel(T{trialStartEndDigiTriggerNumbers(1)}))-1],[1 min(10,numel(T{trialStartEndDigiTriggerNumbers(1)}))-2]))-100,avgTrialDuration);
         Atmp=permute(Atmp,[3 1 2]);Atmp=Atmp(:);
-        medAtmp = fastmedfilt1d(Atmp,round(frameSamples*0.8));
+        medAtmp = fastmedfilt1d(Atmp,round(frameSamples*0.8));%extract the median signal
         eva = evalclusters(medAtmp,'kmeans','DaviesBouldin','KList',[2:4]);
         [idx,cent] = kmeans(medAtmp,eva.OptimalK,'Replicates',5);
         cent=sort(cent);
@@ -114,12 +115,12 @@ if ~noisyAnalog %if noisy, estimate for each chunk
     f=figure;plot(medAtmp);hold on;line([1 numel(medAtmp)],[transitions(1) transitions(1)]);
 end
 
-%main loop
+%main loop- Analog segement is fetched in each chunk and transitions are extracted
 hWB=waitbar(0,hWB,'Extracting analog diode data from recording...');
 upCross=cell(1,nChunks);
 downCross=cell(1,nChunks);
 for i=1:nChunks
-    [A,t_ms]=dataRecordingObj.getAnalogData(analogChNum,chunkStart(i),chunkEnd(i)-chunkStart(i));
+    [A,t_ms]=dataRecordingObj.getAnalogData(analogChNum,chunkStart(i),chunkEnd(i)-chunkStart(i)); %get analog data for the chunk
     if ~noisyAnalog
         A=squeeze(A);
         medA = fastmedfilt1d(A,round(frameSamples*0.8));
@@ -136,7 +137,7 @@ for i=1:nChunks
     upCross{i}=chunkStart(i)+find(medA(1:end-1)<transitions(1) & medA(2:end)>=transitions(1))/Fs*1000;
     downCross{i}=chunkStart(i)+find(medA(1:end-1)>transitions(1) & medA(2:end)<=transitions(1))/Fs*1000;
     %plot(medA(1:5000000));hold on;plot(upCross{i}(1:20)*Fs/1000,medA(round(upCross{i}(1:20)*Fs/1000)),'or');plot(downCross{i}(1:20)*Fs/1000,medA(round(downCross{i}(1:20)*Fs/1000)),'sg')
-    waitbar(i / nChunks);
+    waitbar(i / nChunks); %update progress bar
 end
 close(hWB);
 upCross=cell2mat(upCross');
@@ -144,7 +145,7 @@ downCross=cell2mat(downCross');
 if ~noisyAnalog
     close(f);
 end
-
+%create the frameShifts variable
 udCross{1}=upCross;udCross{2}=downCross;
 frameShifts=udCross;
 
@@ -152,11 +153,11 @@ crossFinal=zeros(2,numel(T{trialStartEndDigiTriggerNumbers(1)}));
 transitionNotFound=zeros(2,numel(T{trialStartEndDigiTriggerNumbers(1)}));
 for i=1:numel(trialStartEndDigiTriggerNumbers)
     tmpTig=T{trialStartEndDigiTriggerNumbers(i)}+delay2Shift;
-    if numel(tmpTig)==numel(udCross{i})
+    if numel(tmpTig)==numel(udCross{i}) %if all transitions were detected
         fprintf('Same number of transitions in session %d diode and triggers, taking diode signal as time stamps\n',trialStartEndDigiTriggerNumbers(i));
         crossFinal(i,:)=udCross{i};
         fprintf('mean difference in lag was %f +- %f',mean(tmpTig'-udCross{i}),std(tmpTig'-udCross{i}));
-    else
+    else %if not
         fprintf('Number of diode transitions in session %d, different from triggers, checking single events\n',trialStartEndDigiTriggerNumbers(i));
         for j=1:numel(T{trialStartEndDigiTriggerNumbers(i)})
             tmpT=udCross{i}(udCross{i}>=(tmpTig(j)-maxFrameDeviation) & udCross{i}<=(tmpTig(j)+maxFrameDeviation));
