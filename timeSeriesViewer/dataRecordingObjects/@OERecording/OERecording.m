9f70f466a55cffc1ff14e10ca4c2e9b596159721
Guy <guyhyman7@gmail.com>
2022-04-20 12:36:12 +0300

    semicolons
    added semicolons to lines I forgot in the original edit, so cNA_index and channelNamesAll won't print to command window during operation.

--- timeSeriesViewer/dataRecordingObjects/@OERecording/OERecording.m
+++ timeSeriesViewer/dataRecordingObjects/@OERecording/OERecording.m
@@ -291,8 +291,8 @@ classdef OERecording < dataRecording
                     for c=1:length(lineEnd)
                         lineSec = section(lineEnd(c)-2:lineEnd(c)-1);
                         Number = str2num(lineSec(regexp(lineSec,'\d')))+1;
-                        cNA_index = cellfun(@(x) strcmp(x{1}(2:end),num2str(Number)),channelNamesAll)
-                        channelNamesAll{cNA_index}{1}=['AD' channelNamesAll{cNA_index}{1}(2:end)]
+                        cNA_index = cellfun(@(x) strcmp(x{1}(2:end),num2str(Number)),channelNamesAll);
+                        channelNamesAll{cNA_index}{1}=['AD' channelNamesAll{cNA_index}{1}(2:end)];
                     end
                 end
                 %AUX
@@ -301,8 +301,8 @@ classdef OERecording < dataRecording
                     for c=1:length(lineEnd)
                         lineSec = section(lineEnd(c)-2:lineEnd(c)-1);
                         Number = str2num(lineSec(regexp(lineSec,'\d')))+1;
-                        cNA_index = cellfun(@(x) strcmp(x{1}(2:end),num2str(Number)),channelNamesAll)
-                        channelNamesAll{cNA_index}{1}=['AU' channelNamesAll{cNA_index}{1}(2:end)]
+                        cNA_index = cellfun(@(x) strcmp(x{1}(2:end),num2str(Number)),channelNamesAll);
+                        channelNamesAll{cNA_index}{1}=['AU' channelNamesAll{cNA_index}{1}(2:end)];
                     end
                 end
                 %CH
@@ -310,8 +310,8 @@ classdef OERecording < dataRecording
                 for c=1:length(lineEnd)
                     lineSec = section(lineEnd(c)-2:lineEnd(c)-1);
                     Number = str2num(lineSec(regexp(lineSec,'\d')))+1;
-                    cNA_index = cellfun(@(x) strcmp(x{1}(2:end),num2str(Number)),channelNamesAll)
-                    channelNamesAll{cNA_index}{1}=['CH' channelNamesAll{cNA_index}{1}(2:end)]
+                    cNA_index = cellfun(@(x) strcmp(x{1}(2:end),num2str(Number)),channelNamesAll);
+                    channelNamesAll{cNA_index}{1}=['CH' channelNamesAll{cNA_index}{1}(2:end)];
                 end
                 channelNumbersAll=cellfun(@(x) str2double(x{1}(3:end)),channelNamesAll,'UniformOutput',1);
             else
